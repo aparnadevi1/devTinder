ecc9443254aa48591b085fe8b488e37797eb7f00
aparnadevigm <aparnach1269@gmail.com>
2024-11-27 00:47:05 +0530

    exploring postman

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
-const express=require("express");
-const app=express()
-app.use("/test",(req,res)=>{
-    res.send("hello from the server");
-})
-app.listen(3000,()=>{
-    console.log("server us ;istenong");
-})
+const express = require("express");
+const app = express();
+// app.get("/user",(req,res)=>{
+//     res.send({firstName:"Mouryaa",lastName:"Devi"})
+// })
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Mourya", lastName: "Devi" });
+});
+app.post("/user", (req, res) => {
+  res.send("data added successfully");
+});
+
+// app.use("/test",(req,res)=>{
+//     res.send("hello from the server");
+// })
+app.listen(3000, () => {
+  console.log("server us ;istenong");
+});
