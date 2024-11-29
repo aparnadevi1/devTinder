fa9c6aeae07720e74fd3469511eff914f2aaccc6
aparnadevigm <aparnach1269@gmail.com>
2024-11-29 17:37:26 +0530

    add a logout api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,64 +1,78 @@
-const express=require("express");
+const express = require("express");
 const { validateSignUpData } = require("../utils/validation");
 const User = require("../models/user");
-const bcrypt=require("bcrypt");
+const bcrypt = require("bcrypt");
 //const app=express();
-const authRouter=express.Router();
+const authRouter = express.Router();
 authRouter.post("/signup", async (req, res) => {
-    // console.log(req.body);
-    // const userObj = {
-    //   firstName: "Akshay",
-    //   lastName: "Saini",
-    //   emailId: "akshay@saini.com",
-    //   password: "akshay@123  ",
-    // };
-  
-    // //saves data to the datadase and returns a promise
-    try {
-      //validates the data
-      validateSignUpData(req);
-  
-      //Encrypting the password
-      //npm package bcrypt
-      const { firstName, lastName, emailId, password } = req.body;
-      const passwordHash = await bcrypt.hash(password, 10);
-      const user = new User({
-        firstName,
-        lastName,
-        emailId,
-        password: passwordHash,
-      });
-  
-      await user.save();
-      res.send("User added Successfully");
-    } catch (err) {
-      res.status(400).send("Error Saving the user" + err.message);
-    }
-  });
+  // console.log(req.body);
+  // const userObj = {
+  //   firstName: "Akshay",
+  //   lastName: "Saini",
+  //   emailId: "akshay@saini.com",
+  //   password: "akshay@123  ",
+  // };
 
-  authRouter.post("/login", async (req, res) => {
-    try {
-      const { emailId, password } = req.body;
-  
-      const user = await User.findOne({ emailId: emailId });
-      if (!user) {
-        throw new Error("Email id not present in DB");
-      }
-      const isPasswordValid = await user.validatePassword(password);
-      if (isPasswordValid) {
-        //create a JWT Token
-        const token = await user.getJWT();
-       // console.log(token);
-  
-        //Add the token  to cookie and send the response back to the user
-        res.cookie("token", token,{expires:new Date(Date.now()+8*3600000)});
-        res.send("Login Successful");
-      } else {
-        throw new Error("Password is not valid");
-      }
-    } catch (err) {
-      res.status(400).send("something went wrong" + err.message);
+  // //saves data to the datadase and returns a promise
+  try {
+    //validates the data
+    validateSignUpData(req);
+
+    //Encrypting the password
+    //npm package bcrypt
+    const { firstName, lastName, emailId, password } = req.body;
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
+    await user.save();
+    res.send("User added Successfully");
+  } catch (err) {
+    res.status(400).send("Error Saving the user" + err.message);
+  }
+});
+
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Email id not present in DB");
     }
-  });
-  
-module.exports=authRouter;
+    const isPasswordValid = await user.validatePassword(password);
+    if (isPasswordValid) {
+      //create a JWT Token
+      const token = await user.getJWT();
+      // console.log(token);
+
+      //Add the token  to cookie and send the response back to the user
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 8 * 3600000),
+      });
+      res.send("Login Successful");
+    } else {
+      throw new Error("Password is not valid");
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong" + err.message);
+  }
+});
+
+authRouter.post("/logout", async (req, res) => {
+  //Do cleanups if present
+  try {
+    res.cookie("token", null, {
+      expires: new Date(Date.now()),
+    });
+    res.send("Logged Out successfully");
+  } catch (err) {
+    res.status(400).send("something went wrong" + err.message);
+  }
+});
+
+module.exports = authRouter;
