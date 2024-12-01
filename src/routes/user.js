359dec8bde3c495baad757dd28e51a40ef66c41a
aparnadevigm <aparnach1269@gmail.com>
2024-12-02 00:24:52 +0530

    added pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -64,6 +64,11 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 userRouter.get("/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
     const connectionRequests = await connectionRequest
       .find({
         $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
@@ -79,7 +84,10 @@ userRouter.get("/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(hideUsersFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(SAVEDATASTRING);
+    })
+      .select(SAVEDATASTRING)
+      .skip(skip)
+      .limit(limit);
     res.send(users);
   } catch {
     res.status(400).json({ message: "Something went wrong" });
